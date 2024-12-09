86c7b92f428d77237b5d713b569c7562bec701d5
tgermer <tgermer@me.com>
2024-12-09 14:46:22 +0100

    Initial Commit

---
+++ SpinTrace/SpinTraceApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinTraceApp.swift
+//  SpinTrace
+//
+//  Created by Tristan Germer on 09.12.24.
+//
+
+import SwiftUI
+
+@main
+struct SpinTraceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
